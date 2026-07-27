ee69957902fd98d0b1b89d3d5e75310a8299d14c
Rockkinkajou581 <151485808+Rockkinkajou581@users.noreply.github.com>
2026-07-27 15:17:32 -0400

    all code, just needs testing

--- train.py
+++ train.py
@@ -1,20 +1,31 @@
 import numpy as np
 import dataloader as dl
 
-def train(input_data, true_data, nn: NueralNetwork):
+def train(input_data, true_data, nn):
     X_training, Y_training, X_validation, Y_validation = dl.split(input_data, true_data)
-    epoch = 5
-    validation_best = None
-    for i in range(epoch):
+    max_epoch = 500
+    validation_best = float('inf')
+    patience = 0
+
+    for epoch in range(max_epoch):
         X, Y = dl.make_batches(X_training, Y_training)
         for X,Y in zip(X,Y):
             nn.gradient_descent(X, Y)
         validation_loss = nn.validate(X_validation, Y_validation)
 
-        if validation_loss < validation_best or validation_best is None:
+        if validation_loss < validation_best:
             validation_best = validation_loss
-            best_W = [x for ]
-    
+            best_W = [W.copy() for W in nn.weights]
+            best_b = [b.copy() for b in nn.weights]
+            patience = 0
+        else:
+            patience += 1
+            if (patience > 10):
+                nn.weights = best_W
+                nn.bias = best_b
+                return
+
+            
     
 
 
